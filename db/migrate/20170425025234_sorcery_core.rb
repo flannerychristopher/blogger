8befc5a612dee8cdef95a8375eaaa26e4caa89f2
flannerychristopher <flannerychristopher@gmail.com>
2017-04-29 13:50:03 +1000

    authentication issue with create user

--- db/migrate/20170425025234_sorcery_core.rb
+++ db/migrate/20170425025234_sorcery_core.rb
@@ -1,10 +1,10 @@
-class SorceryCore < ActiveRecord::Migration[5.0]
+class SorceryCore < ActiveRecord::Migration
   def change
     create_table :authors do |t|
       t.string :username,         :null => false
       t.string :email,            :null => false
       t.string :crypted_password
-      t.string :salt            
+      t.string :salt
 
       t.timestamps                :null => false
     end
